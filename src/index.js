4c3e2dd81e128500f08619ba491fd7c1f975e433
Fouad Matin <fm@fouad.co>
2016-04-20 12:48:45 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const findWithRegex = (regex, contentBlock, callback) => {
+const findWithRegex = (regex, contentBlock) => {
   // Get the text from the contentBlock
   const text = contentBlock.getText();
   let matchArr;
   let start; // eslint-disable-line
+  let matches = []
   // Go through all matches in the text and return the indizes to the callback
   while ((matchArr = regex.exec(text)) !== null) { // eslint-disable-line
     start = matchArr.index;
-    callback(start, start + matchArr[0].length);
+    matches.push([start, start + matchArr[0].length]);
   }
+
+  return matches;
 };
 
 export default findWithRegex;
